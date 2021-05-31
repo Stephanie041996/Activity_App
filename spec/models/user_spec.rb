15f61f6287895c77d22fddf51523f8feeef33c16
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-05-31 23:07:13 +0200

    Add unit test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  let(:user) { User.create(name: 'user') }
+  let(:activities) { User.reflect_on_association(:activities).macro }
+  let(:groups) { User.reflect_on_association(:groups).macro }
+  
+
+  it 'checks and validates user' do
+    @user = User.create(name: 'user')
+    expect(@user).to be_valid
+  end
+
+  it 'checks if activities association is has_many' do
+    expect(activities).to eq(:has_many)
+  end
+
+  it 'checks if group association is :has_many' do
+    expect(groups).to eq(:has_many)
+  end
+
+  context 'validation test' do
+    it 'user name is present' do
+      user = User.new().save
+      expect(user).to eq(false)
+    end
+
+    it 'makes sure the minimum length of name is at least 3' do
+      user = User.new(name: 'user').save
+      expect(user).to eq(true)
+    end
+
+    it 'should save successfully' do
+      user = User.new(name: 'User1').save
+      expect(user) == true
+    end
+  end
+  
+
+ 
+
+  
+
+ 
+  end
